e037efea68428473e0b06c1ba6b917057a3873e6
Varad Yelamkar <varadyelamkar3@gmail.com>
2023-10-15 03:34:30 +0530

    First basic Flask Server

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("Home.html")
+
+if __name__ ==  "__main__":
+    app.run(debug=True)
